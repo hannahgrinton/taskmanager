a64dccea526160fddab5b57cfa47e0dbf86a9685
Hannah Grinton <hannahgrinton@gmail.com>
2020-02-07 12:56:52 -0400

    ready to move on

--- Controllers/homeController.php
+++ Controllers/homeController.php
@@ -1,20 +1,51 @@
 <?
+require_once('../Models/homeManager.php');
+
+$homeManager = new HomeManager();
 
 
 if($_POST['action'] == "backlog") {
-    backlog();
-} else if($_POST['action'] == "progress") {
-    progress();
+    backlog($homeManager);
+} else if ($_POST['action'] == "progress") {
+    progress($homeManager);
+} else if ($_POST['action'] == "done") {
+    done($homeManager);
 } else if ($_POST['action'] == "move") {
-    move();
+    move($homeManager);
 }
-
-function backlog() {
+// ----------------------------------------- functions for adding to categories
+function backlog($myHomeManager) {
+    $homeManager = $myHomeManager;
+    if(isset($_POST['item'])) {
+        $category = 1;
+        $text = $_POST['item'];
+        $homeManager->add($text, $category);
+    }
     header('Location: ../Views/index.php');
 }
-function progress() {
+function progress($myHomeManager) {
+    $homeManager = $myHomeManager;
+    if(isset($_POST['item'])) {
+        $category = 2;
+        $text = $_POST['item'];
+        $homeManager->add($text, $category);
+    }
     header('Location: ../Views/index.php');
 }
-function move() {
+function done($myHomeManager) {
+    $homeManager = $myHomeManager;
+    if(isset($_POST['item'])) {
+        $category = 3;
+        $text = $_POST['item'];
+        $homeManager->add($text, $category);
+    }
+    header('Location: ../Views/index.php');
+}
+// ----------------------------------------- moving right to next category
+function move($myHomeManager) {
+    $homeManager = $myHomeManager;
+    if (isset($_POST['id'])) {
+        $id = $homeManager->move($id);
+    }
     header('Location: ../Views/index.php');
 }
--- Models/homeManager.php
+++ Models/homeManager.php
@@ -1,12 +1,37 @@
 <?php
+require_once('item.php');
+require_once('dbConnect.php');
 
 class HomeManager {
     // ------------------------------------------------ private variables
+    private $_backlog = [];
+    private $_progress = [];
+    private $_done = [];
+    private $_all = [];
     // ------------------------------------------------ constructor
     public function __contruct() {
 
     }
     // ------------------------------------------------ gets / sets
+    public function getBacklog() {
+        return $_backlog;
+    }
+    public function getProgress() {
+        return $_progress;
+    }
+    public function getDone() {
+        return $_done;
+    }
+    public function getAll() {
+        return $_all;
+    }
     // ------------------------------------------------ private methods
     // ------------------------------------------------ public methods
+    //create a new task item
+    public function add($text, $category) {
+        $item = new Item($text, $category);
+    }
+    //move the task item to the category to the right
+    public function move($id) {
+    }
 }
