a0fc83503f3f04cd38658357841a6992b290674e
Hannah Grinton <hannahgrinton@gmail.com>
2020-02-07 11:47:20 -0400

    finished styling the items - onto class work

--- Controllers/homeController.php
+++ Controllers/homeController.php
@@ -3,8 +3,18 @@
 
 if($_POST['action'] == "backlog") {
     backlog();
+} else if($_POST['action'] == "progress") {
+    progress();
+} else if ($_POST['action'] == "move") {
+    move();
 }
 
 function backlog() {
-    header('Location: ../Views/index.html');
+    header('Location: ../Views/index.php');
+}
+function progress() {
+    header('Location: ../Views/index.php');
+}
+function move() {
+    header('Location: ../Views/index.php');
 }
