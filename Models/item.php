b36ad3c4ad891b9897fe9e2bb477c35c72939f79
Hannah Grinton <hannahgrinton@gmail.com>
2020-02-07 12:24:42 -0400

    item class is ready

--- Models/item.php
+++ Models/item.php
@@ -2,14 +2,34 @@
 
 class Item {
     // ------------------------------------------------ private variables
-    private $_text;
-    private $_category;
+    private $_text; //task user entered
+    private $_category; //int representing category
+                        //1 = backlog
+                        //2 = in-progress
+                        //3 = done
+    private $_id; //unique identifier
     // ------------------------------------------------ contructor
     public function __construct($myText, $myCategory) {
+        $_id = null;
         $_text = $myText;
         $_category = $myCategory;
     }
     // ------------------------------------------------ gets / sets
+    public function getText() {
+        return $_text;
+    }
+    public function setText($myText) {
+        $_text = $myText;
+    }
+    public function getCategory() {
+        return $_category;
+    }
+    public function setCategory($myCategory) {
+        $_category = $myCategory;
+    }
+    public function getId() {
+        return $_id;
+    }
     // ------------------------------------------------ private methods
     // ------------------------------------------------ public methods
 }
